5b7bb3e53d0b51a90e0932589338d9696aea634b
Sean C <theseanconn@gmail.com>
2026-05-22 16:03:40 -0400

    Fill stats-year API with airframes, top pilots, and landing leaderboards

---
+++ api/stats-year.config.example.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'api_token' => '',
+    'debug' => false,
+    'db' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => 'vuscgco1_finalphp',
+        'username' => 'YOUR_DB_USER',
+        'password' => 'YOUR_DB_PASSWORD',
+    ],
+];
